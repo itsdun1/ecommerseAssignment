429772c6f2aecb6f5a8dc1a69da000ffb8fb4cca
itsdun1 <adityakalengp@gmail.com>
2019-04-12 02:05:18 +0530

    =solved some issue1

--- conn/conn.js
+++ conn/conn.js
@@ -2,5 +2,5 @@ var mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 var l ="mongodb://127.0.0.1:27017/asse";
 var x = "mongodb://itsdun:Aditya99@ds135796.mlab.com:35796/asse"
-mongoose.connect( l, { useNewUrlParser: true });
+mongoose.connect( x, { useNewUrlParser: true });
 module.exports = {mongoose};    
